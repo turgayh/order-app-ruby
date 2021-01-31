969640c065245a5666ef50940d2083fcb2069420
hakanturgay <turgayh@itu.edu.tr>
2021-02-01 01:22:11 +0300

    UI improvent && category validate

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,7 @@
 class Category < ApplicationRecord
+    validate :check_category_name 
+
+    def check_category_name
+        errors.add(:category_name , "is empty!!") if category_name.length() == 0 
+    end 
 end
